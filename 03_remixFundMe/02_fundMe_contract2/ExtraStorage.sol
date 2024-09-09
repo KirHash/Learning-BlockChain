82c7087fef7e49adcb2ae8cd201dc2f71bbcf059
KirHash <104854308+KirHash@users.noreply.github.com>
2024-09-09 17:21:15 +0530

    Update ExtraStorage.sol

--- 03_remixFundMe/02_fundMe_contract2/ExtraStorage.sol
+++ 03_remixFundMe/02_fundMe_contract2/ExtraStorage.sol
@@ -4,10 +4,8 @@ pragma solidity ^0.8.0;
 import "./SimpleStorage.sol";
 
 
-// Inheritance of SimpleStorage.sol
 contract ExtraStorage is SimpleStorage {
     
-    // Overriding functions using two keywords virtual and override
     function store(uint256 _favoriteNumber) public override {
         favoriteNumber = _favoriteNumber + 5;
     }
