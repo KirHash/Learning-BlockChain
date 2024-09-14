1c7defad569adb86c62f305a04e90050344df58d
KirHash <104854308+KirHash@users.noreply.github.com>
2024-09-15 00:19:53 +0530

    Update hardhat.config.js

--- 05_hardhat-simple-storage/hardhat.config.js
+++ 05_hardhat-simple-storage/hardhat.config.js
@@ -1,13 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomicfoundation/hardhat-verify");
-require("./tasks/block-number")
+require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const ETHERSCAN_API_KEY  = process.env.ETHERSCAN_API_KEY ;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey";
+const ETHERSCAN_API_KEY  = process.env.ETHERSCAN_API_KEY || "key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -29,4 +32,14 @@ module.exports = {
     // Your API key for Etherscan
     apiKey: ETHERSCAN_API_KEY ,
   },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
 };
+
+// Hardhat looks for this file(hardhat.config) and it is kind of entry point for any task that we can and starts with hardhat
